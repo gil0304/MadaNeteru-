ddbeea1dcb346ca9f886aa3f58ba5a35c8655a62
gil <ryogo5.0304@gmail.com>
2026-06-28 19:18:00 +0900

    Initial Commit

---
+++ MadaNeteru?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MadaNeteru?
+//
+//  Created by 落合遼梧 on 2026/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MadaNeteru?/MadaNeteru_App.swift
@@ -0,0 +1,17 @@
+//
+//  MadaNeteru_App.swift
+//  MadaNeteru?
+//
+//  Created by 落合遼梧 on 2026/06/28.
+//
+
+import SwiftUI
+
+@main
+struct MadaNeteru_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
